65a500bed7a545fbd6fe3ac7fa9462ce185c5773
SAEKI Yoshiyasu <laclef.yoshiyasu@gmail.com>
2012-07-07 14:28:40 +0900

    stow

--- Library/Formula/stow.rb
+++ Library/Formula/stow.rb
@@ -2,14 +2,12 @@ require 'formula'
 
 class Stow < Formula
   homepage 'http://www.gnu.org/software/stow/'
-  url 'http://ftp.gnu.org/gnu/stow/stow-2.2.0.tar.gz'
-  mirror 'http://ftpmirror.gnu.org/stow/stow-2.2.0.tar.gz'
-  md5 '902564d502975f353d3b0514a21863b1'
+  url 'http://ftpmirror.gnu.org/stow/stow-2.2.0.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/stow/stow-2.2.0.tar.gz'
+  sha1 'b95091be6ebbbac8c5e5112d6d063299c5eefff2'
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--infodir=#{info}",
-                          "--mandir=#{man}"
+    system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 end
